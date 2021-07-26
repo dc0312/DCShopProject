f4194017d992d596c38f5c265496a4c3b5e300d0
dc0312 <deepakchaturvedi0312@gmail.com>
2021-07-26 17:25:10 +0530

    Initial commit

---
+++ DCShopWebParent/DCShopBackend/src/main/java/com/dc/shop/admin/DcShopBackendApplication.java
@@ -0,0 +1,16 @@
+package com.dc.shop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class DcShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcShopBackendApplication.class, args);
+	}
+
+}
---
+++ DCShopWebParent/DCShopBackend/src/test/java/com/dc/shop/admin/DcShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.shop.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
